fbd7274383a9dd0ca359bc404e47f2474f8df550
Heewon Ryu <janehryu@gmail.com>
2015-02-12 22:38:06 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,6 +17,7 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -27,6 +28,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"  + idNumber, addProject);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	'<img src="' + result['image'] + '"class="detailsImage">' +
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p</a>';
+
+		$('#project' + result['id']+' .details').html(projectHTML);
 }
 
 /*
@@ -35,4 +48,19 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", randomColor);
 }
+
+function randomColor(result) {
+	console.log(result);
+
+	var colors=result["colors"]["hex"];
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+}
+
